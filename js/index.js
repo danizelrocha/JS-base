93e413551e3be18c3b05ca7889fd09fab0aedefe
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-10 15:13:06 -0300

    Aula 4
    var

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ console.log("Oi isso é um console log");
 function minhafuncao(){
     document.write(5+5);
 }
+
+/* Buscar resultados no inspecionar  dentro a opção console */
+
+var a =1;
+var b = 5;
+var c = a + b;
+console.log(c);
+
+var PESSOA = "Daniel";
+var pessoa = "Rocha";
+console.log(PESSOA); /* ele vai buscar por pessoa ou PESSOA individualmente ele entende cada declaração como unica */
