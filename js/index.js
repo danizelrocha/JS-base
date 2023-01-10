cc52d3c0f0bcdf085bad58e1b2ea3d778ff0b979
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-10 16:48:43 -0300

    Aula 5 var
    explicando

--- js/index.js
+++ js/index.js
@@ -3,13 +3,45 @@ console.log("Oi isso é um console log");
 function minhafuncao(){
     document.write(5+5);
 }
+/*
+Vamos entedre as variaveis
+Variáveis são recipientes onde podemos armazenar informações que 
+podem "variar", ou seja , podem ter qualquer tipo de valor
+No Javascript temos 3 palavras chaves para declarar qualquer tipo de variáveis:
+-> var = Podem ser redeclaradas ou seja sempre vai valer a ultima declaração.
+-> let
+-> const 
+*/
+var pote = "Bombom";
+alert(pote);
 
 /* Buscar resultados no inspecionar  dentro a opção console */
-
 var a =1;
 var b = 5;
 var c = a + b;
 console.log(c);
+
+//Declaração de variáveis//
+var a,b,c;
+
+var nome, sobrenome, nomeCompleto, idade, soma;
+
+//Atribuição dos valores//
+a = 1;
+b = 4;
+c = a + b;
+console.log(c);
+
+nome="Daniel";
+sobrenome = "Alves da Rocha";
+idade = 45;
+nomeCompleto = nome +" "+ sobrenome;
+
+soma= idade;
+pessoa = nome + " " + sobrenome + " " + soma;
+
+document.getElementById("texto").innerHTML = pessoa;
+
 
 var PESSOA = "Daniel";
 var pessoa = "Rocha";
