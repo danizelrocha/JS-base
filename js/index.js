a1d9d370419fea99fd6e9d140c339b1cc3f90fd6
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-14 16:35:31 -0300

    Laços
    numeros

--- js/index.js
+++ js/index.js
@@ -1026,3 +1026,6 @@ function diaDaSemana(){
 Laços oferencem um jeito fácil e rápido de executar uma ação de 
 repetidas vezes.*/
 
+for (let i=0; i<10001; i++) {
+    document.getElementById("teste").innerHTML += i + ", ";
+}
