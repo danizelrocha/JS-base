397da447604e0e6f0d1be2e2cea1c8d251822d47
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-15 21:16:48 -0300

    Evento_setInteval
    Cronometro_regressivo

--- js/index.js
+++ js/index.js
@@ -1113,21 +1113,40 @@ function pararContagem() {
 
 
 //-----------------setInteval----------------
-
-
+/* 
+// PROGRESSIVO
 function ativarContagem(){
     tempo = setInterval(function (){
      var cronometro = document.getElementById('tempo').innerHTML;
      var soma = parseInt(cronometro) + 1;
      document.getElementById('tempo'). innerHTML = soma;
     }, 1000);
-
 }   
-
 
 function pararContagem() {
     clearInterval(tempo);
    // document.getElementById('tempo'). innerHTML = "Parou a contagem.";
+}     */
 
 
+
+//REGRESSIVO
+
+function ativarContagem(){
+    tempo = setInterval(function (){
+     var cronometro = document.getElementById('tempo').innerHTML;
+     var soma = parseInt(cronometro) - 1;
+
+     if(soma === 0){
+        document.getElementById('tempo').innerHTML = "Tempo esgotado."
+        pararContagem()
+     }else{
+        document.getElementById('tempo').innerHTML = soma;
+     }
+    }, 1000);
+}   
+
+function pararContagem() {
+    clearInterval(tempo);
+   // document.getElementById('tempo'). innerHTML = "Parou a contagem.";
 }    
